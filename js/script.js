df7e42e4ad02c3e59556cf1f63f71d91209300a1
Riad mhamud <120044256+riad-mhamud@users.noreply.github.com>
2024-08-18 19:36:13 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,133 @@
+
+
+// example1
+function quiz1(){
+    document.getElementById('quiz1').innerHTML="Java Script";
+}
+
+
+// example2
+function quiz2(){
+    document.getElementById('quiz2').innerHTML="syntax is grammer or way of writing code";
+}
+
+
+// example3
+function quiz3(){
+    document.getElementById('quiz3').innerHTML=" there are 4 types of variables in javacript";
+}
+
+
+// example4
+function quiz4(){
+    document.getElementById('quiz4').innerHTML=" statement is line of meaningful code ";
+}
+
+// example11
+function color1(){
+    document.getElementById('color1').style.color="red";
+}
+
+// example12
+function fontsize1(){
+    document.getElementById('fontsize1').style.fontSize="16px";
+}
+function fontsize2(){
+    document.getElementById('fontsize1').style.fontSize="24px";
+}
+function fontsize3(){
+    document.getElementById('fontsize1').style.fontSize="36px";
+}
+function fontsize4(){
+    document.getElementById('fontsize1').style.fontSize="48px";
+}
+function fontsize5(){
+    document.getElementById('fontsize1').style.fontSize="64px";
+}
+function fontsize6(){
+    document.getElementById('fontsize1').style.fontSize="72px";
+}
+
+// example13
+function blue(){
+    document.getElementById('textcolor').style.color="blue";
+}
+
+function ash(){
+    document.getElementById('textcolor').style.color="#aaaaaa";
+}
+
+function red(){
+    document.getElementById('textcolor').style.color="red";
+}
+
+function green(){
+    document.getElementById('textcolor').style.color="green";
+}
+
+function lightblue(){
+    document.getElementById('textcolor').style.color="lightblue";
+}
+
+function offwhite(){
+    document.getElementById('textcolor').style.color="#ffffff";
+}
+
+function dark(){
+    document.getElementById('textcolor').style.color="yellow";
+}
+
+
+// math problem 
+
+let number1, number2, result;
+number1=5;
+number2=6;
+number1+=number2;
+
+let a=15;
+let b=5;
+let number3=a-b;
+let number4=a*b;
+let number5=a/b;
+let number6=a**b;
+let number7=a%b;
+ 
+
+
+function mathProblem(){
+    document.getElementById('math').innerHTML = number1;
+}
+
+//mathexample2
+function mathProblem2(){
+    document.getElementById('math2').innerHTML = number3;
+} 
+
+//mathexample3
+function mathProblem4(){
+    document.getElementById('math4').innerHTML = number4;
+}
+
+//mathexample5
+function mathProblem5(){
+    document.getElementById('math5').innerHTML = number5;
+}
+
+//mathexample6
+function mathProblem6(){
+    document.getElementById('math6').innerHTML = number6;
+}
+
+//mathexample7
+function mathProblem7(){
+    document.getElementById('math7').innerHTML = number7;
+}
+
+//time
+const Datenow= new Date ();
+function dateNow(){
+    document.getElementById("dateNOW").innerHTML=Datenow;
+}
+
+
